c581bb6ce8e3e0907ec0db0f6520f355f531fc1c
stella-yin567 <shiruiyin11@gmail.com>
2026-05-11 18:46:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nginx -c /app/nginx.conf
-OMBRE_BUCKETS_DIR=/app/buckets_kk OMBRE_PORT=8001 python server.py &
-OMBRE_BUCKETS_DIR=/app/buckets_deepseek OMBRE_PORT=8002 python server.py &
+OMBRE_BUCKETS_DIR=/app/buckets/kk OMBRE_PORT=8001 python server.py &
+OMBRE_BUCKETS_DIR=/app/buckets/deepseek OMBRE_PORT=8002 python server.py &
 wait
